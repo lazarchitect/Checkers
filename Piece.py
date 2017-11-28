e82e3705de85414881345261999895e88277fd8e
Eddie Lazar <eddie.lazar@yahoo.com>
2017-11-27 20:19:30 -0500

    Removed unnecessary comments

--- Piece.py
+++ Piece.py
@@ -5,7 +5,7 @@ class Piece():
 		self.team = team #gonna be 0 or 1
 		self.x = x
 		self.y = y
-		#note that being next to a wall or edge (unless you have climb or f2s) will limit this list. or ally presence
+		#note that being next to a wall or edge will limit this list. or ally presence
 		self.board = b
 		self.king = False #at first
 		self.imagePath = "redPiece.png" if self.team == 0 else "blackPiece.png"
